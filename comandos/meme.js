ba1a32000144eff391d1305eb212b0590d8a835b
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2021-04-10 20:08:03 -0300

    Comandos ?meme extremamente simplificado

--- comandos/meme.js
+++ comandos/meme.js
@@ -3,45 +3,17 @@ const randomPuppy = require('random-puppy');
 
 async function SendMemes(message, argvs) {
 
-    // Aqui vamos definir as váriaveis...
-    // Img está vazia, já que depois eu vou colocar o url do meme..
-    // let Local é apenas para saber de onde eu tirei o meme...
-    // SubReddit array com alguns SubReddit...
-    // RandomSub, pega um Sub aleatório da array dos SubReddit...
-    let img = '';
-    let local = '';
     let SubReddit = ['dankmeme', 'me_irl', 'FellowKids', 'memes', 'niceguys', 'Tinder', 'BikiniBottomTwitter', '2meirl4meirl', 'ShitPostCrusaders']
     let RandomSub = SubReddit[Math.floor(Math.random() * SubReddit.length)];
-    // Agora vamos ver de qual SubReddit vamos pegar a imagem...
-    // Se o usuário não inseriu um sub, pegamos um random da array...
-    // Se o usuário inseriu, vemos se ele existe, e se não, avisamos o usuário...
-    if (!argvs[0]) {
-
-        img = await randomPuppy(RandomSub);
-        local = RandomSub;
-
-    } else if (argvs.length > 0) {
-
-        img = await randomPuppy(argvs[0]);
-
-        if (img === undefined) {
-
-            message.channel.send(`${message.author}, **Você escreveu um SubReddit errado ! **`)
-            return
-        } else {
-            img = await randomPuppy(argvs[0])
-            local = argvs[0]
-        }
-    }
-
-    // Agora criamo um Embed simples, e depois enviamos...
+    let WhereGetImg = argvs[0] || RandomSub
+    if (await randomPuppy(WhereGetImg) === undefined) WhereGetImg = RandomSub;
+    let ImgSubReddit = WhereGetImg;
     const MemeEmbed = new Discord.MessageEmbed()
-        .setTitle(`Meme retirado do SubReddit: **${local}** `)
+        .setTitle(`Meme retirado do SubReddit: **${ImgSubReddit}** `)
         .setColor('#00FBFC')
-        .setImage(img);
+        .setImage(await randomPuppy(WhereGetImg));
 
-    message.channel.send(MemeEmbed)
-
+    message.channel.send(MemeEmbed);
 }
 
 
